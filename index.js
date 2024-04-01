cf4d37ac080d8b73c459f5d984666616d2f86bc4
mjasirpp <mjasirpp@gmail.com>
2024-04-01 12:38:09 +0530

    updated all files

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const mediaPlayerServer = jsonServer.create()
 
 const router = jsonServer.router("db.json")
 
-const middleWares = jsonServer.defaults()
+const middlewares = jsonServer.defaults()
 
-const port = 8000 || process.env.PORT
+const port = 4000 || process.env.PORT
 
-mediaPlayerServer.use(middleWares)
+mediaPlayerServer.use(middlewares)
 mediaPlayerServer.use(router)
 
 mediaPlayerServer.listen(port,()=>{
-    console.log(`port : ${port}`);
+    console.log(`Media Player server started at port ${port}, and waiting for client request!!!`);
 })
